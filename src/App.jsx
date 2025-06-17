4969d0fa1434d269298172f88788cffaac58660e
Rabyn-Collab <babynshrestha64@gmail.com>
2025-06-17 08:11:46 +0545

    changes some

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import { createBrowserRouter } from "react-router"
-import { RouterProvider } from "react-router-dom";
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Contact from "./pages/contact/Contact";
-import Page1 from "./pages/home/nested/Page1";
-import Page2 from "./pages/home/nested/Page2";
 
 export default function App() {
+  return (
+    <div>
 
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-      children: [
-        {
-          index: true,
-          element: <Page1 />
-        },
-        {
-          path: 'page-2',
-          element: <Page2 />
-        }
-      ]
-    },
-    {
-      path: 'about',
-      element: <About />
-    },
-    {
-      path: 'contact',
-      element: <Contact />
-    }
-    //lsdk
-
-  ]);
-
-
-
-  return <RouterProvider router={router} />
+    </div>
+  )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
-import Header from '../../components/Header'
-import { NavLink, Outlet } from 'react-router'
 
 export default function Home() {
   return (
     <div>
 
-      <Header />
-      <h1 className='font-bold'>This is homepage</h1>
-
-
-      <nav className='space-x-5'>
-        <NavLink to={'/'}>Page1</NavLink>
-        <NavLink to={'/page-2'}>Page2</NavLink>
-      </nav>
-
-      <Outlet />
-
-
-
-
+      <h1>Hello jee</h1>
 
     </div>
   )
