25b74d4f9c043b79ff6c760280320d7494bea44a
Rabyn-Collab <babynshrestha64@gmail.com>
2025-06-20 09:06:30 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from "react";
+
+
 
 export default function Home() {
+
+
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount((prev) => prev + 1);
+  }
+
+  console.log('render');
   return (
-    <div>
+    <div className="p-5">
+
+      <h1>{count}</h1>
+
+      <button onClick={handleCount}>Increment</button>
+
+
+
+
 
     </div>
   )
