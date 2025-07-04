289155a1ad1f96397a00de4af50de38d00a59a0c
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-04 09:09:31 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,54 +1,10 @@
-import { faker } from '@faker-js/faker';
-import { Button } from '@material-tailwind/react';
-import {
-  List,
-  ListItem,
-  ListItemSuffix,
-  Chip,
-  Card,
-} from "@material-tailwind/react";
-import { useState } from 'react';
+import React from 'react'
+import MealCategory from '../meal_category/MealCategory'
 
 export default function Home() {
-
-  const [data, setData] = useState([]);
-
-  const handleClick = () => {
-    setData((prev) => [...prev, faker.science.chemicalElement()]);
-  }
-
-  const handleDelete = (symbol) => {
-    setData((prev) => prev.filter((pre) => pre.symbol != symbol))
-
-  }
-
   return (
-    <div className='p-5'>
-
-      <Button onClick={handleClick} className='cursor-pointer'> Add Fake Data</Button>
-
-      {data.length > 0 && <Card className="w-96 mt-6">
-        <List>
-          {data.map((chemical, index) => {
-            return <ListItem key={index}>
-              {chemical.name}
-              <ListItemSuffix className='flex gap-5'>
-                <Chip
-                  value={chemical.symbol}
-                  variant="ghost"
-                  size="sm"
-                  className="rounded-full"
-                />
-                <Button onClick={() => handleDelete(chemical.symbol)} size='sm' className='text-[10px]' color='pink'>Delete</Button>
-              </ListItemSuffix>
-            </ListItem>
-          })}
-
-
-        </List>
-      </Card>
-      }
-
+    <div>
+      <MealCategory />
 
     </div>
   )
