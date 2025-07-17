92ee030ece5e53b8ae6c690a627c90026a2a14e7
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-17 08:16:01 +0545

    sabai add gare

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router"
 import RootLayOut from "./components/RootLayOut";
-import UserList from "./features/users/UserList";
-import UserForm from "./features/users/UserForm";
-import UpdateForm from "./features/users/UpdateForm";
 
 
 export default function App() {
@@ -14,16 +11,9 @@ export default function App() {
       children: [
         {
           index: true,
-          element: <UserList />
+
         },
-        {
-          path: 'add-user',
-          element: <UserForm />
-        },
-        {
-          path: 'update-user/:id',
-          element: <UpdateForm />
-        }
+
 
       ]
     },
